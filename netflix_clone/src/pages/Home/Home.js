2f8b617fba6220044d26a0212324384b95cb1da5
Usmanalebe <abukewser21@gmail.com>
2025-06-16 22:15:35 +0300

    banner
    banner list

--- netflix_clone/src/pages/Home/Home.js
+++ netflix_clone/src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "../../Components/Header/header";
 import Footer from "../../Components/Footer/footer";
+import Banner from "../../banner/banner";
 
 const Home = () => {
   return (
     <>
       <Header />
+      <Banner />
       <Footer />
     </>
   );
