e7ab8f0ddedba956ea23929e57dd7e01f5e78876
Usmanalebe <abukewser21@gmail.com>
2025-06-17 14:50:42 +0300

    row
    row

---
+++ netflix_clone/src/Components/Rows/RowList/RowList.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Row from "../Row";
+const RowList = () => {
+  return (
+    <>
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+      <Row />
+    </>
+  );
+};
+
+export default RowList;
--- netflix_clone/src/pages/Home/Home.js
+++ netflix_clone/src/pages/Home/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../../Components/Header/header";
 import Footer from "../../Components/Footer/footer";
 import Banner from "../../banner/banner";
+import RowList from "../../Components/Rows/RowList";
 
 const Home = () => {
   return (
     <>
       <Header />
       <Banner />
+      <RowList />
       <Footer />
     </>
   );
